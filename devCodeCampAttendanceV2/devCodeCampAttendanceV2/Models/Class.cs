c97b4036b4b1a34ec05f3fcbb6b826fd276ce8b3
tprange11 <tprange@gmail.com>
2018-05-08 13:48:32 -0500

    Added "Class" class

---
+++ devCodeCampAttendanceV2/devCodeCampAttendanceV2/Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace devCodeCampAttendanceV2.Models
+{
+
+    public class Class
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+
+
+
+    }
+}
