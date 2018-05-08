cc846aa324c99bc0ba191ecc58faec6bcf85b9bf
tprange11 <tprange@gmail.com>
2018-05-08 14:38:52 -0500

    Installed Hangfire

--- devCodeCampAttendanceV2/devCodeCampAttendanceV2/Startup.cs
+++ devCodeCampAttendanceV2/devCodeCampAttendanceV2/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Hangfire;
 
 [assembly: OwinStartupAttribute(typeof(devCodeCampAttendanceV2.Startup))]
 namespace devCodeCampAttendanceV2
@@ -9,6 +10,10 @@ namespace devCodeCampAttendanceV2
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage("DefaultConnection");
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
         }
     }
 }
