7d542463abc42ef178f8e688f6b18a8c43a636dc
tprange11 <tprange@gmail.com>
2018-05-08 13:58:18 -0500

    Added AccessPointAddress class

---
+++ devCodeCampAttendanceV2/devCodeCampAttendanceV2/Models/AccessPointAddress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace devCodeCampAttendanceV2.Models
+{
+    public class AccessPointAddress
+    {
+        public int ID { get; set; }
+        public string AccessPointName { get; set; }
+        public string MAC { get; set; }
+    }
+
+}
