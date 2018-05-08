9f8fb1413719ab8b85c61fc8e30ba5273c0f937f
Ellie Studnicka <e.studnicka@gmail.com>
2018-05-08 13:19:19 -0500

    Committing project

---
+++ devCodeCampAttendance/devCodeCampAttendance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(devCodeCampAttendance.Startup))]
+namespace devCodeCampAttendance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
