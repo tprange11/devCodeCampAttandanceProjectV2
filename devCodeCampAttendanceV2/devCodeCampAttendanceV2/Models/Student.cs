a7adabe7b4a4b6e0f8007448d5daf4bc92b41a32
tprange11 <tprange@gmail.com>
2018-05-08 13:51:28 -0500

    Added Student Class

---
+++ devCodeCampAttendanceV2/devCodeCampAttendanceV2/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace devCodeCampAttendanceV2.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
