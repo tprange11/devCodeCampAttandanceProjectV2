66755db3af5bf2ac2985bbd8db3d6fb46204e02c
tprange11 <tprange@gmail.com>
2018-05-08 14:00:45 -0500

    ClassStudent junction class

---
+++ devCodeCampAttendanceV2/devCodeCampAttendanceV2/Models/ClassStudent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace devCodeCampAttendanceV2.Models
+{
+    public class ClassStudent
+    {
+        public int ClassID { get; set; }
+        public virtual Class Class { get; set; }
+        public int StudentID { get; set; }
+        public virtual Student Student { get; set; }
+
+    }
+}
