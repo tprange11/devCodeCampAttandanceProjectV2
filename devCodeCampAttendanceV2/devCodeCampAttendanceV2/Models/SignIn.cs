3796bfa5932b283b415166d7da50e7473156e002
tprange11 <tprange@gmail.com>
2018-05-08 13:56:18 -0500

    Added SignIn class

---
+++ devCodeCampAttendanceV2/devCodeCampAttendanceV2/Models/SignIn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace devCodeCampAttendanceV2.Models
+{
+    public class SignIn
+    {
+        public int ID { get; set; }
+        public int StudentID { get; set; }
+        public virtual Student Student { get; set; }
+
+        public int ClassID { get; set; }
+        public virtual Class Class { get; set; }
+        public DateTime Date { get; set; }
+        public bool Late { get; set; }
+    }
+}
