d2457cfbbeffddaa5a1773799baa8f0bb701269f
tprange11 <tprange@gmail.com>
2018-05-08 14:02:20 -0500

    Added Slack Id to Student

--- devCodeCampAttendanceV2/devCodeCampAttendanceV2/Models/Student.cs
+++ devCodeCampAttendanceV2/devCodeCampAttendanceV2/Models/Student.cs
@@ -10,6 +10,8 @@ namespace devCodeCampAttendanceV2.Models
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string SlackID { get; set; }
+
 
 
 
